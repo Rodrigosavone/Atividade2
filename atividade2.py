594d6ddd026c7dedae0c08f26604f7672f374365
LeonardoSa <33468255+LeoonardoSa@users.noreply.github.com>
2021-10-20 18:15:23 -0300

    Parte Final

--- atividade2.py
+++ atividade2.py
@@ -11,10 +11,19 @@ def main():
         elif (opcao == 3):
             listarUsuarioPorOrdemAlfabetica(listaDados)
             opcao = menu()
+        elif (opcao == 4):
+            buscarUsuario(listaDados)
+            opcao = menu()
+        elif (opcao == 5):
+            removerUsuario(listaDados)
+            opcao = menu()
+        elif (opcao == 6):
+            alterarNome(listaDados)
+            opcao = menu()
         else:
             print('Opção invalida escolha novamente:')
             opcao = menu()
-
+        
 def menu():
     print('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
     print('Sistema de Inscrições')
@@ -33,7 +42,7 @@ def menu():
 def cadastro(listaDados):
     dados = dict()
     dados['nome'] = input('Nome completo: ').upper()
-    dados['email'] = input('E-mail: ').upper()
+    dados['email'] = input('E-mail: ')
     listaDados.append(dados)
 
 def listarUsuariosPorOrdemCadastro(listaDados):
@@ -44,6 +53,40 @@ def listarUsuarioPorOrdemAlfabetica(listaDados):
     for usuarios in sorted(listaDados, key = lambda k: k['nome']):
         print('Nome:', usuarios['nome'] , 'E-mail:', usuarios['email'])
 
+def buscarUsuario(listaDados):
+    nome = input('Nome: ')
+    find = False
+    for buscaNome in listaDados:
+        if(buscaNome['nome'] == nome.upper()):
+            print('Usuário encontrado')
+            print('Nome:', buscaNome['nome'], 'E-mail:', buscaNome['email'])
+            find = True
+            break
+    if find == False:    
+        print('Usuário não encontrado')     
+
+def removerUsuario(listaDados):
+    email = input('E-mail: ')
+    remove = False
+    for usuarioDeletado in listaDados:
+        if (usuarioDeletado['email'] == email):
+            print('Usuário removido: ', usuarioDeletado['nome'])
+            listaDados.remove(usuarioDeletado)
+            remove = True
+    if remove == False:    
+        print('Usuário não encontrado')   
+        
+def alterarNome(listaDados):
+    email = input('E-mail: ')
+    alter = False
+    for dados in listaDados:
+        if(dados['email'] == email):
+            print('Nome:', dados['nome'] , 'E-mail:', dados['email'])
+            nome = input('Digite o nome que deseja alterar: ')
+            dados['nome'] = nome
+            alter = True
+    if alter == False:    
+        print('Usuário não encontrado') 
 
 if __name__ == '__main__':
     main()
